46fbaf1b64aba16f4ad1ebd657dbbd5feacc8bf4
ivan <ivanwyf@foxmail.com>
2021-03-31 14:30:33 +0800

    2021/3/31 modify noise dataset, add 'uniform+flip'

--- utils/dataset.py
+++ utils/dataset.py
@@ -2,25 +2,52 @@
 2021/3/9
 create noise dataset
 """
-import random
 
 import matplotlib.pyplot as plt
+import numpy as np
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms, utils
 
 
 class MNISTNoisy(datasets.MNIST):
-    def __init__(self, root, train=True, transform=None, target_transform=None, download=False, noise_rate=0.0):
+    def __init__(self, root, train=True, transform=None, target_transform=None, download=False, noise_rate=0.0, ):
         super().__init__(root, train=train, transform=transform, target_transform=target_transform, download=download)
-        self.noise_targets = self.targets
         self.noise_rate = noise_rate
-        self.create_symmetric_label_noise()
+        self.num_classes = 10
+        # self.uniform_mix(self.noise_rate, mixing_ratio=0.5, num_classes=self.num_classes)
+        # self.flip(self.noise_rate, corruption_prob=0.5, num_classes=self.num_classes)
 
-    def create_symmetric_label_noise(self):
-        for i in range(len(self.noise_targets)):
-            if random.random() < self.noise_rate:
-                random_target = random.randint(0, 10)
-                self.noise_targets[i] = random_target
+    # def create_symmetric_label_noise(self):
+    #     for i in range(len(self.targets)):
+    #         if random.random() < self.noise_rate:
+    #             random_target = random.randint(0, 10)
+    #             self.targets[i] = random_target
+
+    def uniform_mix(self, noise_rate, mixing_ratio, num_classes):
+        ntm = mixing_ratio * np.full((num_classes, num_classes), 1 / num_classes) + \
+              (1 - mixing_ratio) * np.eye(num_classes)
+
+        # indices = np.arange(len(self.data))
+        # np.random.shuffle(indices)
+
+        num_noise = int(len(self.data) * noise_rate)
+
+        for i in range(num_noise):
+            self.targets[i] = np.random.choice(num_classes, p=ntm[self.targets[i]])
+
+    def flip(self, noise_rate, corruption_prob, num_classes):
+        ntm = np.eye(num_classes) * (1 - corruption_prob)
+        row_indices = np.arange(num_classes)
+        for i in range(num_classes):
+            ntm[i][np.random.choice(row_indices[row_indices != i])] = corruption_prob
+
+        # indices = np.arange(len(self.data))
+        # np.random.shuffle(indices)
+
+        num_noise = int(len(self.data) * noise_rate)
+
+        for i in range(num_noise):
+            self.targets[i] = np.random.choice(num_classes, p=ntm[self.targets[i]])
 
     def __len__(self):
         return super().__len__()
@@ -34,7 +61,10 @@ if __name__ == '__main__':
         transforms.ToTensor(),
         transforms.Normalize(mean=(0.1307,), std=(0.3081,))
     ])
+
     mnist_noisy = MNISTNoisy(noise_rate=0.5, transform=transform, root='../dataset', train=True, download=True)
+    mnist_noisy.uniform_mix(noise_rate=1.0, mixing_ratio=0.5, num_classes=10)
+    mnist_noisy.flip(noise_rate=1.0, corruption_prob=0.5, num_classes=10)
 
     noisy_loader = DataLoader(
         mnist_noisy, batch_size=64, shuffle=False)
